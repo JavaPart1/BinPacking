05d97b7bbd0599180f48d662c99080bbac010025
Marc J <john.doe.gmail.com>
2020-07-01 12:03:47 +0200

    Start

---
+++ src/BinClass/BinContainer.java
@@ -0,0 +1,12 @@
+package BinClass;
+
+import java.util.ArrayList;
+
+public class BinContainer {
+    private double maxWeight = 10.0;
+    private ArrayList<Object> objects = new ArrayList<>();
+
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+}
---
+++ src/BinClass/Object.java
@@ -0,0 +1,13 @@
+package BinClass;
+
+public class Object {
+    private double weight;
+
+    public Object(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
